93730550a0b2ce16977bc405855955b16600d67e
_Nattapat.05 <natota4801@gmail.com>
2024-12-25 10:45:55 +0700

    Add node js
    add modules

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";  // นำเข้า Link จาก react-router-dom
 
 function Navbar() {
     return (
         <div className="row m-0">
             <nav className="navbar navbar-expand-lg navbar-dark bg-blue fixed-top">
                 <div className="container">
-                    <a className="navbar-brand" href="#"><i class="fa-solid fa-bookmark me-2"></i> Aurora Resort</a>
+                    <a className="navbar-brand" href="#">
+                        <i className="fa-solid fa-bookmark me-2"></i> Aurora Resort
+                    </a>
                     <button className="navbar-toggler">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <a className="nav-link text-white" href="#">หน้าแรก</a>
+                                <Link className="nav-link text-white" to="/">หน้าแรก</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-white" href="#">ห้องพัก</a>
+                                <Link className="nav-link text-white" to="/room">ห้องพัก</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-white" href="#">ติดต่อเรา</a>
+                                <Link className="nav-link text-white" to="/contact">ติดต่อเรา</Link>
                             </li>
                         </ul>
                     </div>
